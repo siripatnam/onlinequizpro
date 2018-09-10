ca0505684518bc9cd9ecc2f2c120eaee9c2714af
Akruthi Mamillapalli <41600121+Akruthi22@users.noreply.github.com>
2018-09-10 15:11:20 -0400

    initial commit

---
+++ src/main/java/com/onlinequiz/pro/ProApplication.java
@@ -0,0 +1,12 @@
+package com.onlinequiz.pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProApplication.class, args);
+    }
+}
